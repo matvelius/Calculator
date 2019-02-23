1bc5225593274e3a3acfcf0dde8d785a6f8eb9cc
Matvey Kostukovsky <matvelius@gmail.com>
2019-02-23 08:51:48 -0600

    initial commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    // comment
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
