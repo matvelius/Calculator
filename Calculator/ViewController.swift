eab8cf7b669b5ae3c904a3821421546a309819b6
eskew <teskinder1@gmail.com>
2019-02-23 10:01:38 -0600

    make num works

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,7 +1,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var num1:String = ""
+    @IBOutlet var resultView: UITextView!
+    @IBAction func numButton(_ sender: UIButton) {
+        num1.append(Character(sender.title(for: .normal)!))
+        resultView.text = num1
+    }
+    
+    @IBAction func opButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
